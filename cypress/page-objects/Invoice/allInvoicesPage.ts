3d2b6328dd75ddb61087de25e2ae436b31f5c6db
Shanny Cohen Bashir <shanny.cohen.bashir@workiz.com>
2022-07-12 10:23:44 +0300

    space

--- cypress/page-objects/Invoice/allInvoicesPage.ts
+++ cypress/page-objects/Invoice/allInvoicesPage.ts
@@ -8,7 +8,6 @@ export class AllInvoicesPage {
         //Need to find a better solution to get text without the texts of the child element!!!
         const idToSearch = invoiceId.toString();
         cy.get('.rt-tbody .rt-td:nth-child(2)', {timeout: 10000}).should('contain.text',idToSearch).each(($el, index, $list) => {
-
             if($el.text() == idToSearch)
             {
                 expect($el.text()).to.be.eq(idToSearch);
